16ffc54c28a9757bf77fe2b6b630233c31b3f26e
vamshi7589 <vamshi7589@gmail.com>
2020-11-04 22:41:14 +0530

    added: valid first name program added

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,1 +1,12 @@
 echo "**********welcome to user registration problem***********"
+function getFirstName(){
+
+read -p "Enter First name :" fname
+fnamePattern="^[A-Z][a-zA-Z]{2,}$"
+
+if [[ $fname =~ $fnamePattern ]]
+then
+        echo "First Name is valid"
+else
+        echo "First Name is invalid"
+fi
