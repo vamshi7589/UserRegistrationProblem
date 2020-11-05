d2f8fb75dcf0f3b54ded912ce4bb047b74941923
vamshi7589 <vamshi7589@gmail.com>
2020-11-05 14:04:18 +0530

    added: updated email program to validate all  sample mails

--- userRegistration.sh
+++ userRegistration.sh
@@ -28,7 +28,7 @@ fi
 function getEmail(){
 
 read -p "Enter Email address :" email
-emailPattern="^[0-9a-zA-Z]+[._+-]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-z]{2}){0,1}$"
+emailPattern="^[0-9a-zA-Z]+[._+-]{0,1}[0-9a-zA-Z]+[@][0-9a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-z]{2,3}){0,1}$"
 
 if [[ $email =~ $emailPattern ]]
 then
